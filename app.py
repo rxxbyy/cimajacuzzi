f80260619a478ea210da9b643d13c30b8cd6c01f
Manuel <manuel.rubio.bastida@uabc.edu.mx>
2023-05-20 22:00:14 -0700

    Adding register forms using FlaskWTF

--- app.py
+++ app.py
@@ -2,34 +2,40 @@ from typing import Callable, Any
 import os
 import secrets
 import sqlite3
+
 from flask import (Flask, render_template, g, session, request,
     redirect, url_for)
-from forms import RegisterForm
-
-__all__ = ['app']
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', secrets.token_urlsafe())
 
-# Configuring database
-DATABASE = f'{os.path.abspath(os.getcwd())}/cimajacuzzi.db'
+#
+# Setting server database
+#
+DATABASE_PATH = f'{os.path.abspath(os.getcwd())}/cimajacuzzi.db'
 
 def get_db() -> object | Any:
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = sqlite3.connect(DATABASE_PATH)
     db.row_factory = sqlite3.Row
 
     return db
 
 @app.teardown_appcontext
 def close_connection(expection) -> None:
+    """ Close database connection """
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
+# 
+# Flask Endpoints
+# 
 @app.route('/')
 def index() -> Callable:
+    """ Route server endpoint. It serves 'index.html' template. """
     return render_template('index.html', session=session)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -37,9 +43,8 @@ def register() -> Callable:
     reg_form = RegisterForm()
 
     if request.method == "POST" and reg_form.validate_on_submit():
-        session['username'] = request.form['username']
+        
         return redirect(url_for('index'))
-
     return render_template('register.html', reg_form=reg_form)
 
 @app.route('/logout', methods=['GET'])
--- init_db.py
+++ init_db.py
@@ -1,11 +1,14 @@
+#!/usr/bin/env python
 from app import app, get_db
 
 def init_db():
+    """ Initialize schemas on database """
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as FILE:
             db.cursor().executescript(FILE.read())
         db.commit()
 
+# Make sure this script can only be executed directly 
 if __name__ == '__main__':
     init_db()
